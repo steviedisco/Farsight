edc44d89ee818bb8e33b36da2bb3a5f8d9690dde
Steve Driscoll <steviedisco@gmail.com>
2017-12-27 04:44:58 +0000

    initial unit test setup

---
+++ Farsight.Server.Duplication.Test/src/capturetests.cpp
@@ -0,0 +1,60 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace FarsightServerDuplicationTest
+{		
+	TEST_CLASS(CaptureTests)
+	{
+	public:
+		
+		TEST_METHOD(CaptureSingleFrame)
+		{
+		}
+
+		TEST_METHOD(PersistSingleFrame)
+		{
+		}
+
+		TEST_METHOD(StreamSingleFrame)
+		{
+		}
+
+		TEST_METHOD(CaptureMultipleFrames)
+		{
+		}
+
+		TEST_METHOD(PersistMultipleFrames)
+		{
+		}
+
+		TEST_METHOD(StreamMultipleFrames)
+		{
+		}
+
+		TEST_METHOD(ScaleFrame)
+		{
+		}
+
+		TEST_METHOD(ScaleStream)
+		{
+		}
+
+		TEST_METHOD(ThirtyFPSStream)
+		{
+		}
+
+		TEST_METHOD(SixtyFPSStream)
+		{
+		}
+
+		TEST_METHOD(LowBitrate)
+		{
+		}
+
+		TEST_METHOD(HighBitrate)
+		{
+		}
+	};
+}
